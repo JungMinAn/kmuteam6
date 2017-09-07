2662fb62584a9787f905eb1a9adedcd18066c289
JungMin An <qqqwkd@kookmin.ac.kr>
2017-09-07 11:40:15 +0900

    fix1

--- 20171638-송지영-assignment2.py
+++ 20171638-송지영-assignment2.py
@@ -3,8 +3,10 @@ while n is not -1:
     factorial = 1
     for i in range(1, n+1):
         factorial = factorial*i
-        if i == n:
-            print(n,"! = ",factorial)
-            n=int(input("Enter a number: "))
+    if n<-1:
+        n=int(input("Enter a number: "))
+    else:
+        print(n,"! = ",factorial)
+        n=int(input("Enter a number: "))
 # kmuteam6
 # kmuteam6

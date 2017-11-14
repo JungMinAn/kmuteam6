bd39ea74d22c1a69665ad3d3c8a12af5be269807
JungMinAn <qqqwkd@kookmin.ac.kr>
2017-11-14 11:15:37 +0900

    assignment8-calcfunc

---
+++ calcFunctions.py
@@ -0,0 +1,36 @@
+from math import factorial as fact, sqrt
+
+def factorial(numStr):
+    try:
+        n = int(numStr)
+        r = str(fact(n))
+    except:
+        r = 'Error!'
+    return r
+
+def decToBin(numStr):
+    try:
+        n = int(numStr)
+        r = bin(n)[2:]
+    except:
+        r = 'Error!'
+    return r
+
+def binToDec(numStr):
+    try:
+        n = int(numStr, 2)
+        r = str(n)
+    except:
+        r = 'Error!'
+    return r
+
+def decToRoman(numStr):
+    return 'dec -> Roman'
+
+def squareRoot(numStr):
+    try:
+        n = int(numStr)
+        r = str(sqrt(n))
+    except:
+        r = 'Error!'
+    return r
